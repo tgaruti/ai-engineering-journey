c3610208655abb532dd067b0ff443017374f8107
tgaruti <thiagogaruti@gmail.com>
2026-02-16 18:41:18 -0300

    Add: Arquivo Go criado

--- go-hello/main.go
+++ go-hello/main.go
@@ -1,1 +1,17 @@
-}) 
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func main() {
+	fmt.Println("==================================================")
+	fmt.Println("🚀 SETUP COMPLETO - GO!")
+	fmt.Println("==================================================")
+	fmt.Printf("\nData: %s\n", time.Now().Format("02/01/2006"))
+	fmt.Printf("Go: %s\n", runtime.Version())
+	fmt.Println("\n✅ Pronto para Semana 1, Dia 1!")
+	fmt.Println("==================================================")
+}
